e8b8b422bd597ba9781e23fc71daf1dd89a7b2a2
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-04-26 20:59:44 +0530

    added admin_profile_update

---
+++ src/main/java/com/ecosupport/ecoguide/Admin_profile_updateController.java
@@ -0,0 +1,40 @@
+package com.ecosupport.ecoguide;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Admin_profile_updateController implements Initializable {
+
+    @FXML
+    private Button back_btn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void return_dashboard(ActionEvent event) throws IOException {
+        Stage sign_in_stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("AdminHomeDashboard.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/admin_animal_add.css");
+        sign_in_stage.setScene(scene);
+        Stage stage = (Stage) back_btn.getScene().getWindow();
+        stage.close();
+        sign_in_stage.show();
+    }
+}
